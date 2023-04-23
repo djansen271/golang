dadc24de77b12fe567d2a6003a31ecfcdb8fd57a
Damon Jansen <djansen271@gmail.com>
2023-04-23 10:27:15 -0700

    Add Hellos to accept multiple names

--- hello/hello.go
+++ hello/hello.go
@@ -9,6 +9,14 @@ import (
 
 func main() {
 	log.SetPrefix("greetings: ")
+
+	names := []string{"Arafael", "Beziquiel", "Curumden"}
+	messages, err1 := greetings.Hellos(names)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	fmt.Println(messages)
+
 	message, err := greetings.Hello("Gladiolaaaa")
 	if err != nil {
 		log.Fatal(err)
